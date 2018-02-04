38fe544f3de83e86979c9b318c80948d09b716df
wansun <rws4987@naver.com>
2018-02-04 15:57:45 +0900

    나이거 모름...

--- 1A-memo/src/memo1/memo1.java
+++ 1A-memo/src/memo1/memo1.java
@@ -2,6 +2,8 @@ package memo1;
 
 public class memo1 {
 	public static void main(String[] args) {
+		
+		//시바 이거 왜 노란줄 뜨냐??
 		if(false) {
 			System.out.println(1);
 		} else {System.out.println(2);
