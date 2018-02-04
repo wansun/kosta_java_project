10058a62d03d69db2f491197396d47ef967da6a1
chunkind <chunkind88@gmail.com>
2018-02-04 16:00:32 +0900

    모른다고..

--- 1A-memo/src/memo1/memo1.java
+++ 1A-memo/src/memo1/memo1.java
@@ -4,6 +4,7 @@ public class memo1 {
 	public static void main(String[] args) {
 		
 		//시바 이거 왜 노란줄 뜨냐??
+		//이건 시바 나도 몰라.
 		if(false) {
 			System.out.println(1);
 		} else {System.out.println(2);
